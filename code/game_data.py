efb4b42f2930bd23f5a9b4baf8fdc97c975feace
BloodAngelOne <maceo.faure.64@gmail.com>
2023-06-04 16:09:06 +0200

    code upload

---
+++ code/game_data.py
@@ -0,0 +1,10 @@
+level_0 = {
+		'terrain': '../levels/0/level_0_terrain.csv',
+        'bush': '../levels/0/level_0_bush.csv',
+        'tree1': '../levels/0/level_0_tree1.csv',
+        'tree2': '../levels/0/level_0_tree2.csv',
+		'crates': '../levels/0/level_0_crates.csv',
+		'enemies':'../levels/0/level_0_enemies.csv',
+		'constraints':'../levels/0/level_0_constraints.csv',
+		'player': '../levels/0/level_0_player.csv',
+		'grass': '../levels/0/level_0_grass.csv'}
---
+++ levels/tilesets/trees.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="trees" tilewidth="205" tileheight="249" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="205" height="249" source="../../Graphics/terrain/trees/tree-big.png"/>
+ </tile>
+ <tile id="1">
+  <image width="179" height="183" source="../../Graphics/terrain/trees/tree-small.png"/>
+ </tile>
+</tileset>
